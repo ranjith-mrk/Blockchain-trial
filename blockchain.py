4a4509775597ac70144f7c0f0e78df314f86462c
ranjith-mrk <innranjith@gmail.com>
2017-10-28 22:59:12 +0530

    Blocks and proof of work

--- blockchain.py
+++ blockchain.py
@@ -3,22 +3,52 @@ class Blockchain(object):
   def __init__(self):
     self.chain = []
     self.current_transactions = []
+    # Create the genesis block
+    self.new_block(previous_hash=1, proof=100)
 
   def new_block(self)
     #Create new block
-    pass
+    block = {
+      'index': len(self.chain) + 1,
+      'timestamp' : time(),
+      'transactions' : self.current_transactions,
+      'proof' : proof,
+      'previous_hash': previous_hash or self.hash(self.chain[-1]),
+    }
+    self.current_transactions = []
 
-  def new_transaction(self)
+    self.chain.append(block)
+    return block
+
+
+  def new_transaction(self, sender, receiver, amount)
     #Create new transaction
-    pass
+    self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+    return self.last_block['index'] + 1
+
+  def proof_of_work(self, last_proof)
+    proof = 0
+    while self.valid_proof(last_proof, proof) is False:
+      proof += 1
+    return proof
+
+  def valid_proof(last_proof, proof)
+    guess = f'{last_proof}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:4] == "0000"
 
   @staticmethod
   def hash(block)
     #Hashes a block
-    pass
+    block_string = json.dumps(block, sort_keys=True).encode()
+    return hashlib.sha256(block_string).hexdigest()
 
   @property
   def last_block(self)
     #Get the last block
-    pass
+    return self.chain[-1]
     

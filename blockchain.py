768c17b3cd2cbd3fd3442c3e76026ecad8f9b4f6
ranjith-mrk <innranjith@gmail.com>
2017-10-28 21:30:20 +0530

    Initial file
    Initial file

---
+++ blockchain.py
@@ -0,0 +1,24 @@
+class Blockchain(object):
+  
+  def __init__(self):
+    self.chain = []
+    self.current_transactions = []
+
+  def new_block(self)
+    #Create new block
+    pass
+
+  def new_transaction(self)
+    #Create new transaction
+    pass
+
+  @staticmethod
+  def hash(block)
+    #Hashes a block
+    pass
+
+  @property
+  def last_block(self)
+    #Get the last block
+    pass
+    
